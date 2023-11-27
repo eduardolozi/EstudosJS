05fab50b8e9d79609299d6317aa66be2f7e7329d
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-11-27 17:47:20 -0300

    promises pt.1

---
+++ Intermediario/Promises/index.js
@@ -0,0 +1,24 @@
+const numeros = document.getElementById("numeros");
+
+let promise = new Promise((sucesso, falha) => {
+  let resultado = true;
+  let tempo = 3000;
+  setTimeout(() => {
+    if (resultado) sucesso("Deu tudo certo!");
+    else falha("Deu tudo errado!");
+  }, tempo);
+});
+
+promise.then((retorno) => {
+  numeros.innerHTML = retorno;
+  numeros.classList.remove("falha");
+  numeros.classList.add("sucesso");
+});
+
+promise.catch((retorno) => {
+  numeros.innerHTML = retorno;
+  numeros.classList.add("falha");
+  numeros.classList.remove("sucesso");
+});
+
+numeros.innerHTML = "Processando...";
