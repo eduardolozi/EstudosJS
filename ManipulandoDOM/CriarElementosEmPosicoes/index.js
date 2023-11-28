1b96f54c72aad5a2247002d8e49e4e216bc239ab
Eduardo Lozano <eduardoellozi2023@gmail.com>
2023-11-27 21:24:00 -0300

    realizando a insercao de cursos

---
+++ ManipulandoDOM/CriarElementosEmPosicoes/index.js
@@ -0,0 +1,86 @@
+const cursos = ["React", "SAPUI5", "C#.NET", "GIT", "SQLSERVER"];
+const caixa_cursos = document.querySelector("#caixaCursos");
+const btnCursoSelecionado = document.querySelector("#btnCursoSelecionado");
+const btnRemoverCurso = document.querySelector("#btnRemoverCurso");
+const btnAntes = document.querySelector("#btnAdicionarNovoCursoAntes");
+const btnDepois = document.querySelector("#btnAdicionarNovoCursoDepois");
+const nomeCursoAdd = document.querySelector("#nomeCurso");
+
+let indice = 0;
+const criaCurso = (nome) => {
+  const novoElemento = document.createElement("div");
+  novoElemento.setAttribute("id", "c" + indice);
+  novoElemento.setAttribute("class", "curso c1");
+  novoElemento.innerHTML = nome;
+
+  const comandos = document.createElement("div");
+  comandos.setAttribute("class", "comandos");
+
+  const rb = document.createElement("input");
+  rb.setAttribute("type", "radio");
+  rb.setAttribute("name", "rbCurso");
+
+  comandos.appendChild(rb);
+  novoElemento.appendChild(comandos);
+  return novoElemento;
+}
+
+cursos.map((elemento, chave) => {
+  const novoElemento = criaCurso(elemento);
+  caixa_cursos.appendChild(novoElemento);
+  indice++;
+});
+
+const radioSelecionado = () => {
+  const todosRadios = [...document.querySelectorAll("input[type=radio]")];
+  let cursoSelecionado = todosRadios.filter((elemento) => {
+    return elemento.checked;
+  });
+  return cursoSelecionado[0];
+};
+
+btnCursoSelecionado.addEventListener("click", () => {
+  const rsSelecionado = radioSelecionado();
+  if (rsSelecionado == undefined) alert("Selecione um curso!");
+  else {
+    const cursoSelecionado = rsSelecionado.parentNode.previousSibling.textContent;
+    alert(`Curso selecionado ${cursoSelecionado}`);
+  }
+});
+
+btnRemoverCurso.addEventListener("click", () => {
+  const rsSelecionado = radioSelecionado();
+  try {
+    const cursoSelecionado = rsSelecionado.parentNode.parentNode;
+    cursoSelecionado.remove();
+  } catch (ex) {
+    alert("Selecione um curso!");
+  }
+});
+
+btnAntes.addEventListener("click", () => {
+  const rsSelecionado = radioSelecionado();
+  if (rsSelecionado == undefined) alert("Selecione um curso!");
+  else {
+    const cursoSelecionado = rsSelecionado.parentNode.parentNode;
+    const novoCurso = criaCurso(nomeCursoAdd.value);
+    caixa_cursos.insertBefore(novoCurso, cursoSelecionado);
+  }
+})
+
+btnDepois.addEventListener("click", () => {
+  const rsSelecionado = radioSelecionado();
+  if (rsSelecionado == undefined) alert("Selecione um curso!");
+  else {
+    const cursoSelecionado = rsSelecionado.parentNode.parentNode;
+    const novoCurso = criaCurso(nomeCursoAdd.value);
+    caixa_cursos.insertBefore(novoCurso, cursoSelecionado.nextSibling);
+  }
+})
+
+// parentNode
+// childNodes[nodeNumber]
+// firstChild
+// lastChild
+// nextSibling
+// previousSibling
